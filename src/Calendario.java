1b815879bae28f94f30fcb32029561044a5d9879
Gustavo Xavier <gustavoxavier94@icloud.com>
2016-04-05 03:32:11 -0300

    adicionadas assinaturas dos metodos do calendario

--- src/Calendario.java
+++ src/Calendario.java
@@ -1,5 +1,30 @@
 // Implementar
 public class Calendario {
 
+	// Agenda uma turma em um determinado dia, a partir de uma certa hora inicial
+	// por uma quantidade determinada de horas
+	public boolean agendar(Turma turma, int dia, int inicio, int horas) {
+		return false;
+	}
+	
+	// Dado um dia e um horario, verifica se a turma esta presente
+	public boolean mesmaTurma(Turma turma, int dia, int hora) {
+		return false;
+	}
+	
+	// Verifica se a turma possui atividades no dia
+	public boolean turmaNoDia(Turma turma, int dia) {
+		return false;
+	}
+	
+	// Dado um dia e um horario, verifica se um usuario esta presente
+	public boolean mesmoUsuario(Usuario usuario, int dia, int hora) {
+		return false;
+	}
+	
+	// Verifica se o usuario ja possui atividades no dia
+	public boolean usuarioNoDia(Usuario usuario, int dia) {
+		return false;
+	}
 	
 }
