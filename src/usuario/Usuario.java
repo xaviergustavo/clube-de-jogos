4c7b6fbf9c4350561cc7bb6e85277ab80b316f7e
Gustavo Xavier <gustavoxavier94@icloud.com>
2016-06-19 17:55:51 -0300

    adicionados campos que nao existiam na primeira implementacao

--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -1,17 +1,51 @@
 package usuario;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import turma.Turma;
+
 public class Usuario {
 	
 	private int id;
 	
 	private String nome;
 	
-	public Usuario(int id, String nome) {
-		this.id = id;
+	private int idade;
+	
+	private String endereco;
+	
+	private long telefone;
+	
+	private Map<Integer, Turma> turmas;
+		
+	public Usuario(String nome, int idade, String endereco, long telefone) {
 		this.nome = nome;
+		this.idade = idade;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.turmas = new HashMap<>();
+	}
+	
+	public String formatoSistemaAntigo() {
+		return String.format("%s,%d,%s,%d", nome, idade, endereco, telefone);
 	}
 	
 	// Getters
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		
+		Usuario other = (Usuario) obj;
+		
+		return this.nome.equals(other.nome);
+	}
 
 	public int getId() {
 		return id;
@@ -20,5 +54,42 @@ public class Usuario {
 	public String getNome() {
 		return nome;
 	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public long getTelefone() {
+		return telefone;
+	}
 	
+	public void imprimirTurmas() {
+		List<Turma> turmas = getTurmas();
+		System.out.println("Quantidade de turmas: " + turmas.size());
+		
+		for (Turma t : turmas) {
+			System.out.println(t);
+			t.getLocal().imprimirHorariosTurma(t);
+		}
+	}
+	
+	public List<Turma> getTurmas() {
+		List<Turma> turmas = new ArrayList<>();
+		for (Entry<Integer, Turma> entry : this.turmas.entrySet()) {
+			turmas.add(entry.getValue());
+		}
+		return turmas;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setTurma(Turma t) {
+		this.turmas.put(t.getId(), t);
+	}
 }
